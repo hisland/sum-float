fe47d41b4ba9b061014003eefc11d6f9a7bc512e
hisland <hisland@qq.com>
2019-07-03 10:45:13 +0800

    js dist update

--- dist/bundle.cjs.js
+++ dist/bundle.cjs.js
@@ -3,30 +3,30 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 function DecimalPart(num1) {
-  let num2;
+  var num2 = void 0;
   if (typeof num1 === 'string') {
     num2 = parseFloat(num1);
   } else if (typeof num1 === 'number') {
     num2 = num1;
   } else {
-    throw Error('param must can cast to number: ' + num1)
+    throw Error('param must can cast to number: ' + num1);
   }
   if (isNaN(num2)) {
-    throw Error('param must can cast to number: ' + num1)
+    throw Error('param must can cast to number: ' + num1);
   }
-  const part2 = (num2 + '').split('.')[1];
-  return part2 ? part2.length : 0
+  var part2 = (num2 + '').split('.')[1];
+  return part2 ? part2.length : 0;
 }
 
 function SumFloat(aa1, bb1) {
-  const len1 = DecimalPart(aa1);
-  const len2 = DecimalPart(bb1);
-  const len3 = Math.max(len1, len2);
+  var len1 = DecimalPart(aa1);
+  var len2 = DecimalPart(bb1);
+  var len3 = Math.max(len1, len2);
   if (len3) {
-    const cc1 = Math.pow(10, len3);
-    return (aa1 * cc1 + bb1 * cc1) / cc1
+    var cc1 = Math.pow(10, len3);
+    return (aa1 * cc1 + bb1 * cc1) / cc1;
   } else {
-    return aa1 - 0 + (bb1 - 0)
+    return aa1 - 0 + (bb1 - 0);
   }
 }
 
